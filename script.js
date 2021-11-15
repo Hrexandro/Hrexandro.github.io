db33d3eaab7ce8b9d30059172120624fa8ec0537
Hrexandro <marcin_nowak_hrx@o2.pl>
2021-11-15 21:08:41 +0100

    add language change animation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ finish changing the language to apply to all text on page
 
 let languageSetToPolish = true
 
-let languageSwitch = document.getElementById("slider-container");
+let languageSwitch = document.getElementById("clicked-switch");
 let title = document.title;
 let about = document.getElementById("about-link");
 let projects = document.getElementById("projects-link");
@@ -21,24 +21,37 @@ let projectLibrary = document.getElementById("project-library");
 let projectsHead = document.getElementById("projects-head");
 let welcomeSection = document.getElementById("welcome-section");
 
-
+let allTranslatedElements = document.getElementsByClassName("translated")
 
 languageSwitch.addEventListener("click", (e) => {
     console.log("event happens")
     e.stopPropagation();
     e.stopImmediatePropagation();
-    e.preventDefault()
+    //e.preventDefault()
 
     console.log(e.target)
     changeLanguage();
-    fillTheAreasWithAppropriateLanguageContent();
+    setTimeout(function() {
+        fillTheAreasWithAppropriateLanguageContent();
+    }, 900);
 })
 
 function changeLanguage() {
     languageSetToPolish = !languageSetToPolish;
+    for (i = 0; i < allTranslatedElements.length; i++) {
+        allTranslatedElements[i].classList.add("disappearing")
+    }
+    // document.addEventListener("transitionend", () => {
+    //     fillTheAreasWithAppropriateLanguageContent();
+    // })
 }
 
 function fillTheAreasWithAppropriateLanguageContent() {
+    // document.removeEventListener("transitionend", () => {
+    //     fillTheAreasWithAppropriateLanguageContent();
+    // })
+    console.log("timeout should start")
+
     if (languageSetToPolish === true) {
         about.innerText = "o mnie";
         projects.innerText = "projekty";
@@ -48,6 +61,12 @@ function fillTheAreasWithAppropriateLanguageContent() {
         projects.innerText = "projects";
         contact.innerText = "contact";
     }
+
+    for (i = 0; i < allTranslatedElements.length; i++) {
+        allTranslatedElements[i].classList.remove("disappearing")
+    }
+
+
 }
 
 fillTheAreasWithAppropriateLanguageContent();
