bc196043e47c50c7ebf4f3c0cd2225ba9fd74ce9
Hrexandro <marcin_nowak_hrx@o2.pl>
2021-11-16 20:37:33 +0100

    finish language switching

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ let projectCalculator = document.getElementById("project-calculator");
 let projectLibrary = document.getElementById("project-library");
 let projectsHead = document.getElementById("projects-head");
 let welcomeSection = document.getElementById("welcome-section");
+let jobTitle = document.getElementById("job-title")
+let descriptionLineOne = document.getElementById("description-line-one");
+let descriptionLineTwo = document.getElementById("description-line-two");
 
 let allTranslatedElements = document.getElementsByClassName("translated")
 
@@ -56,10 +59,25 @@ function fillTheAreasWithAppropriateLanguageContent() {
         about.innerText = "o mnie";
         projects.innerText = "projekty";
         contact.innerText = "kontakt";
+        jobTitle.innerText = "Webdev i tłumacz języka angielskiego";
+        descriptionLineOne.innerText = "Jestem web developerem ze znajomością HTML, CSS i JavaScript, tworzę strony internetowe oraz aplikacje sieciowe. Poniżej można zapoznać się z kilkoma z moich projektów.";
+        descriptionLineTwo.innerText = "Zajmuję się również tłumaczeniami z języka angielskiego.";
+        projectsHead.innerText = "MOJE PROJEKTY";
+        projectEtchASketch.innerText = "Znikopis";
+        projectCalculator.innerText = "Kalkulator";
+        projectLibrary.innerText = "Biblioteka";
+
     } else {
         about.innerText = "about";
         projects.innerText = "projects";
         contact.innerText = "contact";
+        jobTitle.innerText = "Webdev and translator";
+        descriptionLineOne.innerText = "I'm a web developer experienced with HTML, CSS, and JavaScript. I make websites and web applications. You can check out some of my projects below";
+        descriptionLineTwo.innerText = "I also perform English-Polish translation.";
+        projectsHead.innerText = "MY PROJECTS";
+        projectEtchASketch.innerText = "Etch-a-sketch";
+        projectCalculator.innerText = "Calculator";
+        projectLibrary.innerText = "Library";
     }
 
     for (i = 0; i < allTranslatedElements.length; i++) {
